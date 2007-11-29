24c1101fee6e1d3c521c2146abc42bab080ecb6a
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-11-29 05:28:30 -0800

    I really should be sleeping instead of doing this right now.

--- toilet.c
+++ toilet.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "toilet.h"
+#include "hash_map.h"
+
+/* This initial implementation of Toilet stores databases using the file system.
+ * Each gtable gets a subdirectory in the database directory, which itself must
+ * have a file called "toilet-version" containing the single line "0". Each
+ * gtable directory has a directory called "columns" containing files named
+ * after each possible column in the gtable, and these files store two pieces of
+ * information: the type of the column, and the number of rows which have values
+ * for that column. Each gtable directory also has a set of subdirectory trees
+ * containing rows; the row ID (a 32-bit number for now) is written in
+ * hexadecimal and each two digits form a directory name. The last such
+ * subdirectory contains a directory for each key in the row, and in these
+ * directories are files containing the values for that key, one per file,
+ * named starting at "0" and continuing for as many values as there are. */
 
 /* This function returns a toilet pointer. That is, a sign like this:
  * 
@@ -16,6 +31,52 @@
 
 toilet * toilet_open(const char * path)
 {
+	int path_fd, cwd;
+	toilet * toilet;
+	
+	cwd = open(".", 0);
+	if(cwd < 0)
+		return NULL;
+	if(chdir(path) < 0)
+		goto close_1;
+	path_fd = open(".", 0);
+	if(path_fd < 0)
+		goto cwd;
+	
+	toilet = malloc(sizeof(*toilet));
+	if(!toilet)
+		goto close_2;
+	toilet->path = strdup(path);
+	if(!toilet->path)
+		goto free_1;
+	toilet->path_fd = path_fd;
+	toilet->gtables = hash_map_create_str();
+	if(!toilet->gtables)
+		goto free_2;
+	toilet->rows = hash_map_create();
+	if(!toilet->rows)
+		goto destroy;
+	
+	/* ... */
+	fchdir(cwd);
+	
+	return toilet;
+	
+	hash_map_destroy(toilet->rows);
+destroy:
+	hash_map_destroy(toilet->gtables);
+free_2:
+	free((void *) toilet->path);
+free_1:
+	/* i.e., not a pay toilet */
+	free(toilet);
+close_2:
+	close(path_fd);
+cwd:
+	fchdir(cwd);
+close_1:
+	close(cwd);
+	return NULL;
 }
 
 int toilet_close(toilet * toilet)
