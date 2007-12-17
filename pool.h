260f51bec639bceb32645fd447787af83aa238f8
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-12-17 00:58:17 -0800

    Update miscopied copyrights.

--- pool.h
+++ pool.h
@@ -1,5 +1,5 @@
-/* This file is part of Toilet. Toilet is copyright 2005-2007 The
- * Regents of the University of California. It is distributed under the terms of
+/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+ * of the University of California. It is distributed under the terms of
  * version 2 of the GNU GPL. See the file LICENSE for details. */
 
 #ifndef POOL_H
