16e5648bc539a754ef89b65f1b2ff9f4a869e933
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-12-17 15:56:39 -0800

    Make the the test main() work without my hand-crafted test directory.

--- main.c
+++ main.c
@@ -8,15 +8,18 @@
 
 int main(void)
 {
+	int r;
 	toilet * toilet = toilet_open("test");
 	if(toilet)
-	{
-		int r;
 		toilet_close(toilet);
-		
-		r = toilet_new("test.new");
-		if(r < 0)
-			fprintf(stderr, "Failed to create toilet! (test.new)\n");
+	else
+		fprintf(stderr, "Failed to open toilet! ('test')\n");
+	
+	r = toilet_new("test.new");
+	if(r < 0)
+		fprintf(stderr, "Failed to create toilet! ('test.new')\n");
+	else
+	{
 		toilet = toilet_open("test.new");
 		if(toilet)
 		{
@@ -45,9 +48,8 @@ int main(void)
 			toilet_close(toilet);
 		}
 		else
-			fprintf(stderr, "Failed to open toilet! (test.new)\n");
+			fprintf(stderr, "Failed to open toilet! ('test.new')\n");
 	}
-	else
-		fprintf(stderr, "Failed to open toilet! (test)\n");
+	
 	return 0;
 }
