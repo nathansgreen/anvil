382503b2a4fe18fbfa21fcc7d4f931b5224b1c92
Shant Hovsepian <shant@cs.ucla.edu>
2008-08-08 13:02:15 -0700

    Finish cursor prev and last.

--- toilet++.cpp
+++ toilet++.cpp
@@ -623,16 +623,14 @@ int toilet_cursor_prev(t_cursor * cursor)
 {
 	t_cursor_union safer;
 	safer.cursor = cursor;
-	abort();
-	//return safer.iter->prev();
+	return safer.iter->prev();
 }
 
 int toilet_cursor_last(t_cursor * cursor)
 {
 	t_cursor_union safer;
 	safer.cursor = cursor;
-	abort();
-	//return safer.iter->last();
+	return safer.iter->last();
 }
 
 t_row_id toilet_cursor_row_id(t_cursor * cursor)
