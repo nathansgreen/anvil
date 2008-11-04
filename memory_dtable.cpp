ac18f44dd5b05e1b49eae85d992be1958724e4d1
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2008-11-03 16:18:04 -0800

    Add (untested) memory_dtable. Like journal_dtable, but never writes a log at all.

---
+++ memory_dtable.cpp
@@ -0,0 +1,166 @@
+/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+ * of the University of California. It is distributed under the terms of
+ * version 2 of the GNU GPL. See the file LICENSE for details. */
+
+#include <errno.h>
+
+#include "hack_map.h"
+#include "memory_dtable.h"
+
+bool memory_dtable::iter::valid() const
+{
+	return mit != mdt_source->mdt_map.end();
+}
+
+bool memory_dtable::iter::next()
+{
+	if(mit != mdt_source->mdt_map.end())
+		++mit;
+	return mit != mdt_source->mdt_map.end();
+}
+
+bool memory_dtable::iter::prev()
+{
+	if(mit != mdt_source->mdt_map.begin())
+		--mit;
+	return mit != mdt_source->mdt_map.end();
+}
+
+bool memory_dtable::iter::last()
+{
+	mit = mdt_source->mdt_map.end();
+	if(mit != mdt_source->mdt_map.begin())
+		--mit;
+	return mit != mdt_source->mdt_map.end();
+}
+
+dtype memory_dtable::iter::key() const
+{
+	return mit->first;
+}
+
+bool memory_dtable::iter::seek(const dtype & key)
+{
+	mit = mdt_source->mdt_map.lower_bound(key);
+	if(mit == mdt_source->mdt_map.end())
+		return false;
+	return !mdt_source->mdt_map.key_comp()(key, mit->first);
+}
+
+bool memory_dtable::iter::seek(const dtype_test & test)
+{
+	mit = lower_bound(mdt_source->mdt_map, test);
+	if(mit == mdt_source->mdt_map.end())
+		return false;
+	return !test(mit->first);
+}
+
+metablob memory_dtable::iter::meta() const
+{
+	return mit->second;
+}
+
+blob memory_dtable::iter::value() const
+{
+	return mit->second;
+}
+
+const dtable * memory_dtable::iter::source() const
+{
+	return mdt_source;
+}
+
+dtable::iter * memory_dtable::iterator() const
+{
+	return new iter(this);
+}
+
+blob memory_dtable::lookup(const dtype & key, bool * found) const
+{
+	memory_dtable_hash::const_iterator it = mdt_hash.find(key);
+	if(it != mdt_hash.end())
+	{
+		*found = true;
+		return *(it->second);
+	}
+	*found = false;
+	return blob();
+}
+
+int memory_dtable::insert(const dtype & key, const blob & blob, bool append)
+{
+	if(key.type != ktype || (ktype == dtype::BLOB && !key.blb.exists()))
+		return -EINVAL;
+	/* empty hash table? just add it */
+	if(!mdt_hash.size())
+		return add_node(key, blob);
+	if(append)
+	{
+		memory_dtable_map::iterator end = mdt_map.end();
+		memory_dtable_map::iterator last = end;
+		int c = (--last)->first.compare(key, blob_cmp);
+		/* this is the expected case, if the hint was correct */
+		if(c < 0)
+			return add_node(key, blob, end);
+		if(!c)
+		{
+			last->second = blob;
+			return 0;
+		}
+	}
+	memory_dtable_hash::iterator it = mdt_hash.find(key);
+	if(it != mdt_hash.end())
+	{
+		*(it->second) = blob;
+		return 0;
+	}
+	return add_node(key, blob);
+}
+
+int memory_dtable::remove(const dtype & key)
+{
+	if(full_rm)
+	{
+		if(mdt_hash.erase(key))
+			mdt_map.erase(key);
+		return 0;
+	}
+	return insert(key, blob());
+}
+
+int memory_dtable::init(dtype::ctype key_type, bool full_remove)
+{
+	if(ready)
+		deinit();
+	assert(mdt_map.empty());
+	assert(mdt_hash.empty());
+	assert(!cmp_name);
+	ktype = key_type;
+	ready = true;
+	full_rm = full_remove;
+	return 0;
+}
+
+void memory_dtable::deinit()
+{
+	mdt_hash.clear();
+	mdt_map.clear();
+	dtable::deinit();
+	ready = false;
+}
+
+int memory_dtable::add_node(const dtype & key, const blob & value)
+{
+	blob & map_value = mdt_map[key];
+	map_value = value;
+	mdt_hash[key] = &map_value;
+	return 0;
+}
+
+int memory_dtable::add_node(const dtype & key, const blob & value, const memory_dtable_map::iterator & end)
+{
+	memory_dtable_map::value_type pair(key, value);
+	memory_dtable_map::iterator it = mdt_map.insert(end, pair);
+	mdt_hash[key] = &(it->second);
+	return 0;
+}
---
+++ memory_dtable.h
@@ -0,0 +1,88 @@
+/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+ * of the University of California. It is distributed under the terms of
+ * version 2 of the GNU GPL. See the file LICENSE for details. */
+
+#ifndef __MEMORY_DTABLE_H
+#define __MEMORY_DTABLE_H
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <sys/types.h>
+
+#ifndef __cplusplus
+#error memory_dtable.h is a C++ header file
+#endif
+
+#include <ext/hash_map>
+#include <map>
+
+#include "blob.h"
+#include "istr.h"
+#include "dtable.h"
+
+/* The memory dtable stores data in memory, like the journal dtable, but does
+ * not actually log anything to the journal or write a file itself. When it is
+ * destroyed, the data goes with it. */
+/* Depending on how it is used, it may be preferable either to completely remove
+ * removed keys, or to store nonexistent blobs for those keys (as the journal
+ * dtable does). This is controlled by the "full_remove" init() parameter. */
+
+class memory_dtable : public dtable
+{
+public:
+	virtual iter * iterator() const;
+	virtual blob lookup(const dtype & key, bool * found) const;
+	
+	inline virtual bool writable() const { return true; }
+	virtual int insert(const dtype & key, const blob & blob, bool append = false);
+	virtual int remove(const dtype & key);
+	
+	inline virtual int set_blob_cmp(const blob_comparator * cmp)
+	{
+		/* we merely add this assertion, but it's important */
+		assert(mdt_map.empty() || blob_cmp);
+		return dtable::set_blob_cmp(cmp);
+	}
+	
+	inline memory_dtable() : ready(false), full_rm(false), mdt_map(blob_cmp), mdt_hash(10, blob_cmp, blob_cmp) {}
+	int init(dtype::ctype key_type, bool full_remove);
+	void deinit();
+	inline virtual ~memory_dtable()
+	{
+		if(ready)
+			deinit();
+	}
+	
+private:
+	typedef std::map<dtype, blob, dtype_comparator_refobject> memory_dtable_map;
+	typedef __gnu_cxx::hash_map<const dtype, blob *, dtype_hashing_comparator, dtype_hashing_comparator> memory_dtable_hash;
+	
+	inline int add_node(const dtype & key, const blob & value);
+	inline int add_node(const dtype & key, const blob & value, const memory_dtable_map::iterator & end);
+	
+	class iter : public dtable::iter
+	{
+	public:
+		virtual bool valid() const;
+		virtual bool next();
+		virtual bool prev();
+		virtual bool last();
+		virtual dtype key() const;
+		virtual bool seek(const dtype & key);
+		virtual bool seek(const dtype_test & test);
+		virtual metablob meta() const;
+		virtual blob value() const;
+		virtual const dtable * source() const;
+		inline iter(const memory_dtable * source) : mdt_source(source), mit(source->mdt_map.begin()) {}
+		virtual ~iter() {}
+	private:
+		const memory_dtable * mdt_source;
+		memory_dtable_map::const_iterator mit;
+	};
+	
+	bool ready, full_rm;
+	memory_dtable_map mdt_map;
+	memory_dtable_hash mdt_hash;
+};
+
+#endif /* __MEMORY_DTABLE_H */
