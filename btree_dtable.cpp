ec10ae5effa182fc0562461242f5f32ca3e5cbde
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2008-12-02 02:21:15 -0800

    Add slightly tested btree_dtable write support. It doesn't read it yet.

--- btree_dtable.cpp
+++ btree_dtable.cpp
@@ -9,6 +9,39 @@
 #include "util.h"
 #include "btree_dtable.h"
 
+/* A btree dtable doesn't actually store the data itself - that's up to some
+ * other dtable. What btree dtables do is create optimal btrees for looking up
+ * keys, giving better key lookup speed than a binary search. Rather than log
+ * base 2 pages read from disk, we get log base m+1 where m is the number of
+ * keys stored per page. What the btree actually stores is a map from keys to
+ * indices in the underlying dtable. */
+
+/* The first page of a btree dtable file has a header, btree_dtable_header, and
+ * is otherwise empty. Each subsequent page of the btree has one of two forms;
+ * either an internal page, like this:
+ * 
+ * | page# | <key, index> | page# | <key, index> | ... | page# |
+ * 
+ * or a leaf page, like this:
+ * 
+ * | <key, index> | <key, index> | ... | <key, index> |
+ * 
+ * As page numbers, keys, and indices are all 32 bits, we can fit 341 <key,
+ * index> pairs per 4K internal page, with 342 page numbers between them, and
+ * 512 <key, index> pairs per 4K leaf page. We can tell the difference between
+ * the two page types by the (runtime-known) depth of the page from the root. */
+
+#define BTREE_PAGENO_SIZE sizeof(uint32_t)
+#define BTREE_KEY_SIZE sizeof(uint32_t)
+#define BTREE_INDEX_SIZE sizeof(uint32_t)
+
+#define BTREE_KEY_INDEX_SIZE (BTREE_KEY_SIZE + BTREE_INDEX_SIZE)
+#define BTREE_ENTRY_SIZE (BTREE_PAGENO_SIZE + BTREE_KEY_INDEX_SIZE)
+
+/* integers round down */
+#define BTREE_KEYS_PER_PAGE ((BTREE_PAGE_SIZE - BTREE_PAGENO_SIZE) / BTREE_ENTRY_SIZE)
+#define BTREE_KEYS_PER_LEAF_PAGE (BTREE_PAGE_SIZE / BTREE_KEY_INDEX_SIZE)
+
 btree_dtable::iter::iter(dtable::iter * base, const btree_dtable * source)
 	: base_iter(base), bdt_source(source)
 {
@@ -131,6 +164,7 @@ int btree_dtable::init(int dfd, const char * file, const params & config)
 		return -1;
 	}
 	ktype = base->key_type();
+	assert(ktype == dtype::UINT32);
 	cmp_name = base->get_cmp_name();
 	/* XXX open the btree here */
 	close(bt_dfd);
@@ -145,6 +179,214 @@ void btree_dtable::deinit()
 		base = NULL;
 		dtable::deinit();
 	}
+}
+
+/* returns true if the page fills */
+bool btree_dtable::page_stack::page::append_pointer(size_t pointer)
+{
+	assert(filled + sizeof(uint32_t) <= BTREE_PAGE_SIZE);
+	*(uint32_t *) &data[filled] = pointer;
+	filled += sizeof(uint32_t);
+	return filled == BTREE_PAGE_SIZE;
+}
+
+/* returns true if the page fills */
+bool btree_dtable::page_stack::page::append_record(uint32_t key, size_t index)
+{
+	assert(filled + 2 * sizeof(uint32_t) <= BTREE_PAGE_SIZE);
+	*(uint32_t *) &data[filled] = key;
+	*(uint32_t *) &data[filled += sizeof(uint32_t)] = index;
+	filled += sizeof(uint32_t);
+	return filled == BTREE_PAGE_SIZE;
+}
+
+bool btree_dtable::page_stack::page::write(int fd, size_t page)
+{
+	assert(filled == BTREE_PAGE_SIZE);
+	ssize_t r = pwrite(fd, data, BTREE_PAGE_SIZE, page * BTREE_PAGE_SIZE);
+	if(r != BTREE_PAGE_SIZE)
+		return false;
+	filled = 0;
+	return true;
+}
+
+void btree_dtable::page_stack::page::pad()
+{
+	memset(&data[filled], 0, BTREE_PAGE_SIZE - filled);
+	filled = BTREE_PAGE_SIZE;
+}
+
+btree_dtable::page_stack::page_stack(int fd, size_t key_count)
+	: fd(fd), next_file_page(1), filled(false), flushed(false)
+{
+	depth = btree_depth(key_count);
+	assert(depth > 0);
+	pages = new page[depth];
+	next_depth = depth - 1;
+	
+	header.magic = BTREE_DTABLE_MAGIC;
+	header.version = BTREE_DTABLE_VERSION;
+	header.page_size = BTREE_PAGE_SIZE;
+	header.pageno_size = BTREE_PAGENO_SIZE;
+	header.key_size = BTREE_KEY_SIZE;
+	header.index_size = BTREE_INDEX_SIZE;
+	header.key_type = 1; /* 1 -> uint32 */
+	header.key_count = key_count;
+	header.depth = depth;
+	/* to be filled in later */
+	header.root_page = 0;
+}
+
+btree_dtable::page_stack::~page_stack()
+{
+	if(!flushed)
+		flush();
+	delete[] pages;
+}
+
+int btree_dtable::page_stack::add(uint32_t key, size_t index)
+{
+	assert(!filled && !flushed);
+	if(pages[next_depth].append_record(key, index))
+	{
+		size_t pointer = next_file_page++;
+		if(!pages[next_depth].write(fd, pointer))
+			/* FIXME: do better than this */
+			abort();
+		add(pointer);
+	}
+	else if(next_depth < depth - 1)
+		next_depth++;
+	return 0;
+}
+
+int btree_dtable::page_stack::add(size_t pointer)
+{
+	assert(!filled);
+	if(!next_depth)
+	{
+		header.root_page = pointer;
+		filled = true;
+		return 0;
+	}
+	next_depth--; /* check for -1, bail out (full) */
+	if(pages[next_depth].append_pointer(pointer))
+	{
+		pointer = next_file_page++;
+		if(!pages[next_depth].write(fd, pointer))
+			/* FIXME: do better than this */
+			abort();
+		add(pointer);
+	}
+	return 0;
+}
+
+int btree_dtable::page_stack::flush()
+{
+	ssize_t r;
+	assert(!flushed);
+	if(!filled)
+	{
+		if(pages[next_depth].empty() && next_depth)
+			/* the next page is empty, so don't pad and write it */
+			next_depth--;
+		while(!filled)
+		{
+			size_t pointer = next_file_page++;
+			pages[next_depth].pad();
+			pages[next_depth].write(fd, pointer);
+			/* ultimately, this will set filled */
+			add(pointer);
+		}
+	}
+	r = pwrite(fd, &header, sizeof(header), 0);
+	if(r != sizeof(header))
+		return (r < 0) ? r : -1;
+	flushed = true;
+	return 0;
+}
+
+size_t btree_dtable::page_stack::btree_depth(size_t key_count)
+{
+	size_t internal = 1, leaf = 0, depth = 1;
+	/* there's probably some neat closed form way to do this, but this loop
+	 * should only run log(key_count) times, base BTREE_KEYS_PER_PAGE */
+	/* XXX this will quickly overflow; use larger data types? */
+	while(internal * BTREE_KEYS_PER_PAGE + leaf * BTREE_KEYS_PER_LEAF_PAGE < key_count)
+	{
+		internal += leaf;
+		leaf= internal * BTREE_KEYS_PER_PAGE;
+		depth++;
+	}
+	/* we expect this to be true if the integers did not overflow */
+	assert(depth <= 4);
+	return depth;
+}
+
+int btree_dtable::write_btree(int dfd, const char * name, const dtable * base)
+{
+	/* OK, here's how this works. We find out how many keys there are, and
+	 * figure out what the topology of the btree will be based on that.
+	 * (That is, how deep it is.) Then we do an in-order traversal of the
+	 * virtual btree, filling in the keys during a single iteration over the
+	 * source dtable data.
+	 * 
+	 * We write pages out to the btree file as they fill; because pages
+	 * closer to the root of the tree will only fill after all the pages
+	 * they point at fill, all the downward page number pointers will be
+	 * known when it is time to write out a page. Further, we'll only need
+	 * to keep log(n) pages in memory: the ones from the current position in
+	 * the btree traversal up to the root. (That's base BTREE_KEYS_PER_PAGE
+	 * log, even.)
+	 * 
+	 * After all this is done, the last page written will be the root page,
+	 * so we don't actually need to store its location to the file header.
+	 * Nevertheless, we do anyway, as a precaution. */
+	int r = -1, fd;
+	size_t count = base->size();
+	dtable::iter * base_iter;
+	
+	assert(count != (size_t) -1);
+	
+	/* for the moment, we only support integer keys */
+	if(base->key_type() != dtype::UINT32)
+		return -ENOSYS;
+	
+	fd = openat(dfd, name, O_WRONLY | O_CREAT, 0644);
+	if(fd < 0)
+		return fd;
+	
+	page_stack stack(fd, count);
+	
+	base_iter = base->iterator();
+	if(!base_iter)
+		goto fail_iter;
+	
+	for(size_t index = 0; index < count; index++)
+	{
+		assert(base_iter->valid());
+		dtype key = base_iter->key();
+		assert(key.type == dtype::UINT32);
+		base_iter->next();
+		r = stack.add(key.u32, index);
+		if(r < 0)
+			goto fail_write;
+	}
+	r = stack.flush();
+	if(r < 0)
+		goto fail_write;
+	
+	delete base_iter;
+	close(fd);
+	return 0;
+	
+fail_write:
+	unlinkat(dfd, name, 0);
+	delete base_iter;
+fail_iter:
+	close(fd);
+	unlinkat(dfd, name, 0);
+	return r;
 }
 
 int btree_dtable::create(int dfd, const char * file, const params & config, const dtable * source, const dtable * shadow)
@@ -174,12 +416,18 @@ int btree_dtable::create(int dfd, const char * file, const params & config, cons
 	base_dtable = base->open(bt_dfd, "base", base_config);
 	if(!base_dtable)
 		goto fail_reopen;
-	/* XXX make the btree from base_dtable */
+	
+	r = write_btree(bt_dfd, "btree", base_dtable);
+	if(r < 0)
+		goto fail_write;
+	
 	delete base_dtable;
 	
 	close(bt_dfd);
 	return 0;
 	
+fail_write:
+	delete base_dtable;
 fail_reopen:
 	util::rm_r(bt_dfd, "base");
 fail_create:
--- btree_dtable.h
+++ btree_dtable.h
@@ -20,6 +20,11 @@
 /* The btree dtable must be created with another read-only dtable, and builds a
  * btree key index for it. The base dtable must support indexed access. */
 
+#define BTREE_DTABLE_MAGIC 0xB2815C66
+#define BTREE_DTABLE_VERSION 0
+
+#define BTREE_PAGE_SIZE 4096
+
 class btree_dtable : public dtable
 {
 public:
@@ -54,6 +59,56 @@ public:
 	DECLARE_RO_FACTORY(btree_dtable);
 	
 private:
+	struct btree_dtable_header
+	{
+		uint32_t magic;
+		uint32_t version;
+		uint32_t page_size;
+		uint8_t pageno_size;
+		uint8_t key_size;
+		uint8_t index_size;
+		uint8_t key_type;
+		uint32_t key_count;
+		uint32_t depth;
+		uint32_t root_page;
+	} __attribute__((packed));
+	
+	class page_stack
+	{
+	public:
+		page_stack(int fd, size_t key_count);
+		~page_stack();
+		
+		int add(uint32_t key, size_t index);
+		int flush();
+		
+		static size_t btree_depth(size_t key_count);
+		
+	private:
+		class page
+		{
+		public:
+			inline page() : filled(0) { }
+			inline bool append_pointer(size_t pointer);
+			inline bool append_record(uint32_t key, size_t index);
+			inline bool write(int fd, size_t page);
+			inline bool empty() { return !filled; }
+			inline void pad();
+		private:
+			size_t filled;
+			uint8_t data[BTREE_PAGE_SIZE];
+		};
+		
+		int fd;
+		size_t depth;
+		size_t next_depth, next_file_page;
+		page * pages;
+		btree_dtable_header header;
+		bool filled, flushed;
+		
+		int add(size_t pointer);
+	};
+	
 	class iter : public dtable::iter
 	{
 	public:
@@ -78,6 +133,9 @@ private:
 	};
 	
 	dtable * base;
+	
+	static size_t btree_depth(size_t key_count);
+	static int write_btree(int dfd, const char * name, const dtable * base);
 };
 
 #endif /* __BTREE_DTABLE_H */
