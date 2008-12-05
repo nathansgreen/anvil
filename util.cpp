87122f8ddacd9a02085d4383116294f04b0614cd
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2008-12-05 00:05:01 -0800

    Fix compiler warning on newer compilers.

--- util.cpp
+++ util.cpp
@@ -1,10 +1,10 @@
-#include <sys/stat.h>
 /* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
  * of the University of California. It is distributed under the terms of
  * version 2 of the GNU GPL. See the file LICENSE for details. */
 
 #define _ATFILE_SOURCE
 
+#include <sys/stat.h>
 #include <dirent.h>
 
 #include "openat.h"
