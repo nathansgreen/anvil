nathansgreen/anvil

commit 379a1f7bfa0e47ac4105cf07b0cf9b7f2f6f85fc

Store append hints in journal_dtable logs, and add a new digest feature to managed_dtable.

journal_dtable.cpp
@@ -1,4 +1,4 @@
-/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+/* This file is part of Toilet. Toilet is copyright 2007-2009 The Regents
  * of the University of California. It is distributed under the terms of
  * version 2 of the GNU GPL. See the file LICENSE for details. */
 
@@ -107,6 +107,7 @@ struct jdt_string
 struct jdt_key_u32
 {
 	uint8_t type;
+	uint8_t append;
 	uint32_t key;
 	size_t size;
 	uint8_t data[0];
@@ -116,6 +117,7 @@ struct jdt_key_u32
 struct jdt_key_dbl
 {
 	uint8_t type;
+	uint8_t append;
 	double key;
 	size_t size;
 	uint8_t data[0];
@@ -125,6 +127,7 @@ struct jdt_key_dbl
 struct jdt_key_str
 {
 	uint8_t type;
+	uint8_t append;
 	uint32_t index;
 	size_t size;
 	uint8_t data[0];
@@ -134,6 +137,7 @@ struct jdt_key_str
 struct jdt_key_blob
 {
 	uint8_t type;
+	uint8_t append;
 	size_t key_size;
 	size_t size;
 	/* key stored first, then data */
@@ -202,7 +206,7 @@ template<class T> inline int journal_dtable::log(T * entry, const blob & blob, s
 	return r;
 }
 
-int journal_dtable::log(const dtype & key, const blob & blob)
+int journal_dtable::log(const dtype & key, const blob & blob, bool append)
 {
 	if(ktype == dtype::BLOB && blob_cmp && !cmp_name)
 	{
@@ -220,6 +224,7 @@ int journal_dtable::log(const dtype & key, const blob & blob)
 			if(!entry)
 				return -ENOMEM;
 			entry->type = JDT_KEY_U32;
+			entry->append = append;
 			entry->key = key.u32;
 			return log(entry, blob);
 		}
@@ -229,6 +234,7 @@ int journal_dtable::log(const dtype & key, const blob & blob)
 			if(!entry)
 				return -ENOMEM;
 			entry->type = JDT_KEY_DBL;
+			entry->append = append;
 			entry->key = key.dbl;
 			return log(entry, blob);
 		}
@@ -239,6 +245,7 @@ int journal_dtable::log(const dtype & key, const blob & blob)
 			if(!entry)
 				return -ENOMEM;
 			entry->type = JDT_KEY_STR;
+			entry->append = append;
 			r = add_string(key.str, &entry->index);
 			if(r < 0)
 			{
@@ -253,6 +260,7 @@ int journal_dtable::log(const dtype & key, const blob & blob)
 			if(!entry)
 				return -ENOMEM;
 			entry->type = JDT_KEY_BLOB;
+			entry->append = append;
 			entry->key_size = key.blb.size();
 			if(entry->key_size)
 				memcpy(entry->data, &key.blb[0], entry->key_size);
@@ -267,33 +275,16 @@ int journal_dtable::insert(const dtype & key, const blob & blob, bool append)
 	int r;
 	if(key.type != ktype || (ktype == dtype::BLOB && !key.blb.exists()))
 		return -EINVAL;
-	r = log(key, blob);
+	r = log(key, blob, append);
 	if(r < 0)
 		return r;
-	/* empty hash table? just add it */
-	if(!jdt_hash.size())
-		return add_node(key, blob);
-	if(append)
-	{
-		journal_dtable_map::iterator end = jdt_map.end();
-		journal_dtable_map::iterator last = end;
-		int c = (--last)->first.compare(key, blob_cmp);
-		/* this is the expected case, if the hint was correct */
-		if(c < 0)
-			return add_node(key, blob, end);
-		if(!c)
-		{
-			last->second = blob;
-			return 0;
-		}
-	}
 	journal_dtable_hash::iterator it = jdt_hash.find(key);
 	if(it != jdt_hash.end())
 	{
 		*(it->second) = blob;
 		return 0;
 	}
-	return add_node(key, blob);
+	return add_node(key, blob, append);
 }
 
 int journal_dtable::remove(const dtype & key)
@@ -301,7 +292,7 @@ int journal_dtable::remove(const dtype & key)
 	return insert(key, blob());
 }
 
-int journal_dtable::init(dtype::ctype key_type, sys_journal::listener_id lid, sys_journal * journal)
+int journal_dtable::init(dtype::ctype key_type, sys_journal::listener_id lid, bool always_append, sys_journal * journal)
 {
 	int r;
 	if(id() != sys_journal::NO_ID)
@@ -313,6 +304,7 @@ int journal_dtable::init(dtype::ctype key_type, sys_journal::listener_id lid, sy
 	r = strings.init(true);
 	if(r < 0)
 		return r;
+	this->always_append = always_append;
 	string_index = 0;
 	set_id(lid);
 	set_journal(journal);
@@ -347,24 +339,21 @@ void journal_dtable::deinit()
 	dtable::deinit();
 }
 
-int journal_dtable::add_node(const dtype & key, const blob & value)
-{
-	blob & map_value = jdt_map[key];
-	map_value = value;
-	jdt_hash[key] = &map_value;
-	return 0;
-}
-
-int journal_dtable::add_node(const dtype & key, const blob & value, const journal_dtable_map::iterator & end)
+int journal_dtable::add_node(const dtype & key, const blob & value, bool append)
 {
 	journal_dtable_map::value_type pair(key, value);
-	journal_dtable_map::iterator it = jdt_map.insert(end, pair);
+	journal_dtable_map::iterator it;
+	if(append || always_append)
+		it = jdt_map.insert(jdt_map.end(), pair);
+	else
+		it = jdt_map.insert(pair).first;
 	jdt_hash[key] = &(it->second);
 	return 0;
 }
 
 int journal_dtable::journal_replay(void *& entry, size_t length)
 {
+	blob value;
 	if(cmp_name && !blob_cmp)
 		/* if we need a blob comparator and don't have
 		 * one yet, then don't accept journal entries */
@@ -395,18 +384,18 @@ int journal_dtable::journal_replay(void *& entry, size_t length)
 			jdt_key_u32 * u32 = (jdt_key_u32 *) entry;
 			if(ktype != dtype::UINT32)
 				return -EINVAL;
-			if(u32->size == (size_t) -1)
-				return add_node(u32->key, blob());
-			return add_node(u32->key, blob(u32->size, u32->data));
+			if(u32->size != (size_t) -1)
+				value = blob(u32->size, u32->data);
+			return add_node(u32->key, value, u32->append);
 		}
 		case JDT_KEY_DBL:
 		{
 			jdt_key_dbl * dbl = (jdt_key_dbl *) entry;
 			if(ktype != dtype::DOUBLE)
 				return -EINVAL;
-			if(dbl->size == (size_t) -1)
-				return add_node(dbl->key, blob());
-			return add_node(dbl->key, blob(dbl->size, dbl->data));
+			if(dbl->size != (size_t) -1)
+				value = blob(dbl->size, dbl->data);
+			return add_node(dbl->key, value, dbl->append);
 		}
 		case JDT_KEY_STR:
 		{
@@ -414,17 +403,17 @@ int journal_dtable::journal_replay(void *& entry, size_t length)
 			istr string = strings.lookup(str->index);
 			if(!string || ktype != dtype::STRING)
 				return -EINVAL;
-			if(str->size == (size_t) -1)
-				return add_node(string, blob());
-			return add_node(string, blob(str->size, str->data));
+			if(str->size != (size_t) -1)
+				value = blob(str->size, str->data);
+			return add_node(string, value, str->append);
 		}
 		case JDT_KEY_BLOB:
 		{
 			jdt_key_blob * blb = (jdt_key_blob *) entry;
 			blob key(blb->key_size, blb->data);
-			if(blb->size == (size_t) -1)
-				return add_node(key, blob());
-			return add_node(key, blob(blb->size, &blb->data[blb->key_size]));
+			if(blb->size != (size_t) -1)
+				value = blob(blb->size, &blb->data[blb->key_size]);
+			return add_node(key, value, blb->append);
 		}
 		case JDT_BLOB_CMP:
 		{

journal_dtable.h
@@ -1,4 +1,4 @@
-/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+/* This file is part of Toilet. Toilet is copyright 2007-2009 The Regents
  * of the University of California. It is distributed under the terms of
  * version 2 of the GNU GPL. See the file LICENSE for details. */
 
@@ -32,6 +32,8 @@ public:
 	virtual iter * iterator() const;
 	virtual blob lookup(const dtype & key, bool * found) const;
 	
+	/* journal_dtable supports size() even though it is not otherwise indexable */
+	inline virtual size_t size() const { return jdt_hash.size(); }
 	inline virtual bool writable() const { return true; }
 	virtual int insert(const dtype & key, const blob & blob, bool append = false);
 	virtual int remove(const dtype & key);
@@ -44,7 +46,7 @@ public:
 	}
 	
 	inline journal_dtable() : string_index(0), jdt_map(blob_cmp), jdt_hash(10, blob_cmp, blob_cmp) {}
-	int init(dtype::ctype key_type, sys_journal::listener_id lid, sys_journal * journal = NULL);
+	int init(dtype::ctype key_type, sys_journal::listener_id lid, bool always_append = false, sys_journal * journal = NULL);
 	/* reinitialize, optionally discarding the old entries from the journal */
 	/* NOTE: also clears and releases the blob comparator, if one has been set */
 	int reinit(sys_journal::listener_id lid, bool discard = true);
@@ -61,8 +63,7 @@ private:
 	typedef std::map<dtype, blob, dtype_comparator_refobject> journal_dtable_map;
 	typedef __gnu_cxx::hash_map<const dtype, blob *, dtype_hashing_comparator, dtype_hashing_comparator> journal_dtable_hash;
 	
-	inline int add_node(const dtype & key, const blob & value);
-	inline int add_node(const dtype & key, const blob & value, const journal_dtable_map::iterator & end);
+	inline int add_node(const dtype & key, const blob & value, bool append);
 	
 	class iter : public dtable::iter
 	{
@@ -88,9 +89,10 @@ private:
 	int add_string(const istr & string, uint32_t * index);
 	int log_blob_cmp();
 	template<class T> inline int log(T * entry, const blob & blob, size_t offset = 0);
-	int log(const dtype & key, const blob & blob);
+	int log(const dtype & key, const blob & blob, bool append);
 	
 	stringset strings;
+	bool always_append;
 	uint32_t string_index;
 	journal_dtable_map jdt_map;
 	journal_dtable_hash jdt_hash;

main++.cpp
@@ -663,7 +663,7 @@ int command_bdbtest(int argc, const char * argv[])
 	jid = sys_journal::get_unique_id();
 	if(jid == sys_journal::NO_ID)
 		return -EBUSY;
-	r = jdt.init(dtype::BLOB, jid, NULL);
+	r = jdt.init(dtype::BLOB, jid);
 	printf("jdt.init = %d\n", r);
 	r = tx_end(0);
 	printf("tx_end = %d\n", r);
@@ -733,7 +733,7 @@ int command_blob_cmp(int argc, const char * argv[])
 		jid = sys_journal::get_unique_id();
 		if(jid == sys_journal::NO_ID)
 			return -EBUSY;
-		r = jdt.init(dtype::BLOB, jid, NULL);
+		r = jdt.init(dtype::BLOB, jid);
 		printf("jdt.init = %d\n", r);
 		r = jdt.set_blob_cmp(&reverse);
 		printf("jdt.set_blob_cmp = %d\n", r);

managed_dtable.cpp
@@ -1,4 +1,4 @@
-/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+/* This file is part of Toilet. Toilet is copyright 2007-2009 The Regents
  * of the University of California. It is distributed under the terms of
  * version 2 of the GNU GPL. See the file LICENSE for details. */
 
@@ -18,7 +18,7 @@ int managed_dtable::init(int dfd, const char * name, const params & config, sys_
 	istr fast_config = "fastbase_config";
 	tx_fd meta;
 	off_t meta_off;
-	int r = -1;
+	int r = -1, size;
 	if(md_dfd >= 0)
 		deinit();
 	base = dtable_factory::lookup(config, "base");
@@ -33,6 +33,11 @@ int managed_dtable::init(int dfd, const char * name, const params & config, sys_
 		fast_config = "base_config";
 	if(!config.get(fast_config, &fastbase_config, params()))
 		return -EINVAL;
+	/* NOTE: use of this feature causes iterators to potentially
+	 * become invalid on every insert() or remove() */
+	if(!config.get("digest_size", &size, 0))
+		return -EINVAL;
+	digest_size = size;
 	if(!config.get("digest_on_close", &digest_on_close, false))
 		return -EINVAL;
 	if(!config.get("close_digest_fastbase", &close_digest_fastbase, true))

managed_dtable.h
@@ -1,4 +1,4 @@
-/* This file is part of Toilet. Toilet is copyright 2007-2008 The Regents
+/* This file is part of Toilet. Toilet is copyright 2007-2009 The Regents
  * of the University of California. It is distributed under the terms of
  * version 2 of the GNU GPL. See the file LICENSE for details. */
 
@@ -47,15 +47,23 @@ public:
 	/* send to journal_dtable */
 	inline virtual int insert(const dtype & key, const blob & blob, bool append = false)
 	{
+		int r;
 		if(!blob.exists() && !find(key).exists())
 			return 0;
-		return journal->insert(key, blob, append);
+		r = journal->insert(key, blob, append);
+		if(r >= 0 && digest_size && journal->size() >= digest_size)
+			r = digest();
+		return r;
 	}
 	inline virtual int remove(const dtype & key)
 	{
+		int r;
 		if(!find(key).exists())
 			return 0;
-		return journal->remove(key);
+		r = journal->remove(key);
+		if(r >= 0 && digest_size && journal->size() >= digest_size)
+			r = digest();
+		return r;
 	}
 	
 	/* return the number of disk dtables */
@@ -155,6 +163,7 @@ private:
 	 * -EBUSY and we will need to query it later when the blob comparator is
 	 *  set; we set delayed_query when this case is detected in init() */
 	sys_journal * delayed_query;
+	size_t digest_size;
 	bool digest_on_close, close_digest_fastbase;
 };
 

commit 2897f13c7cfe50717a8c4f2805c868a50dc758af

Fix a bug replaying journal_dtable data introduced in revision 379a1f7bfa0e47ac4105cf07b0cf9b7f2f6f85fc.

journal_dtable.cpp
@@ -278,13 +278,7 @@ int journal_dtable::insert(const dtype & key, const blob & blob, bool append)
 	r = log(key, blob, append);
 	if(r < 0)
 		return r;
-	journal_dtable_hash::iterator it = jdt_hash.find(key);
-	if(it != jdt_hash.end())
-	{
-		*(it->second) = blob;
-		return 0;
-	}
-	return add_node(key, blob, append);
+	return set_node(key, blob, append);
 }
 
 int journal_dtable::remove(const dtype & key)
@@ -351,6 +345,17 @@ int journal_dtable::add_node(const dtype & key, const blob & value, bool append)
 	return 0;
 }
 
+int journal_dtable::set_node(const dtype & key, const blob & value, bool append)
+{
+	journal_dtable_hash::iterator it = jdt_hash.find(key);
+	if(it != jdt_hash.end())
+	{
+		*(it->second) = value;
+		return 0;
+	}
+	return add_node(key, value, append);
+}
+
 int journal_dtable::journal_replay(void *& entry, size_t length)
 {
 	blob value;
@@ -386,7 +391,7 @@ int journal_dtable::journal_replay(void *& entry, size_t length)
 				return -EINVAL;
 			if(u32->size != (size_t) -1)
 				value = blob(u32->size, u32->data);
-			return add_node(u32->key, value, u32->append);
+			return set_node(u32->key, value, u32->append);
 		}
 		case JDT_KEY_DBL:
 		{
@@ -395,7 +400,7 @@ int journal_dtable::journal_replay(void *& entry, size_t length)
 				return -EINVAL;
 			if(dbl->size != (size_t) -1)
 				value = blob(dbl->size, dbl->data);
-			return add_node(dbl->key, value, dbl->append);
+			return set_node(dbl->key, value, dbl->append);
 		}
 		case JDT_KEY_STR:
 		{
@@ -405,7 +410,7 @@ int journal_dtable::journal_replay(void *& entry, size_t length)
 				return -EINVAL;
 			if(str->size != (size_t) -1)
 				value = blob(str->size, str->data);
-			return add_node(string, value, str->append);
+			return set_node(string, value, str->append);
 		}
 		case JDT_KEY_BLOB:
 		{
@@ -413,7 +418,7 @@ int journal_dtable::journal_replay(void *& entry, size_t length)
 			blob key(blb->key_size, blb->data);
 			if(blb->size != (size_t) -1)
 				value = blob(blb->size, &blb->data[blb->key_size]);
-			return add_node(key, value, blb->append);
+			return set_node(key, value, blb->append);
 		}
 		case JDT_BLOB_CMP:
 		{

journal_dtable.h
@@ -64,6 +64,8 @@ private:
 	typedef __gnu_cxx::hash_map<const dtype, blob *, dtype_hashing_comparator, dtype_hashing_comparator> journal_dtable_hash;
 	
 	inline int add_node(const dtype & key, const blob & value, bool append);
+	/* tries to set an existing node, and calls add_node() otherwise */
+	inline int set_node(const dtype & key, const blob & value, bool append);
 	
 	class iter : public iter_source<journal_dtable>
 	{