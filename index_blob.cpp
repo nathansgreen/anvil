6179ca2aa10b354b60c2bf6b90f2fa05fd87db9c
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2009-02-11 21:37:05 -0800

    Add index_blob, like sub_blob for with indexed rather than named columns.

---
+++ index_blob.cpp
@@ -0,0 +1,102 @@
+/* This file is part of Toilet. Toilet is copyright 2007-2009 The Regents
+ * of the University of California. It is distributed under the terms of
+ * version 2 of the GNU GPL. See the file LICENSE for details. */
+
+#include "blob_buffer.h"
+#include "index_blob.h"
+
+index_blob::index_blob(size_t count)
+	: modified(true), resized(true), count(count)
+{
+	indices = new sub[count];
+}
+
+index_blob::index_blob(size_t count, const blob & x)
+	: base(x), modified(false), resized(false), count(count)
+{
+	indices = new sub[count];
+	size_t offset = count * sizeof(uint32_t);
+	for(size_t i = 0; i < count; i++)
+	{
+		uint32_t size = base.index<uint32_t>(i);
+		if(size)
+		{
+			if(--size)
+				indices[i].value = blob(size, &base[offset]);
+			else
+				indices[i].value = blob::empty;
+			offset += size;
+		}
+		else
+			assert(!indices[i].value.exists());
+	}
+}
+
+index_blob::index_blob(const index_blob & x)
+	: modified(false), resized(false), count(x.count)
+{
+	base = x.flatten();
+	indices = new sub[count];
+	for(size_t i = 0; i < count; i++)
+		indices[i].value = x.indices[i].value;
+}
+
+index_blob & index_blob::operator=(const index_blob & x)
+{
+	if(this == &x)
+		return *this;
+	delete[] indices;
+	base = x.flatten();
+	modified = false;
+	resized = false;
+	count = x.count;
+	indices = new sub[count];
+	for(size_t i = 0; i < count; i++)
+		indices[i].value = x.indices[i].value;
+	return *this;
+}
+
+blob index_blob::flatten() const
+{
+	if(!modified)
+		return base;
+	if(!resized)
+	{
+		blob_buffer buffer(base);
+		size_t offset = count * sizeof(uint32_t);
+		/* hopefully avoid copying by breaking sharing before modifying */
+		base = blob();
+		for(size_t i = 0; i < count; i++)
+		{
+			if(indices[i].modified)
+			{
+				buffer.overwrite(offset, indices[i].value);
+				indices[i].modified = false;
+			}
+			offset += indices[i].value.size();
+		}
+		modified = false;
+		base = buffer;
+	}
+	else
+	{
+		size_t offset = count * sizeof(uint32_t);
+		for(size_t i = 0; i < count; i++)
+			offset += indices[i].value.size();
+		blob_buffer buffer(offset);
+		for(size_t i = 0; i < count; i++)
+		{
+			/* we use 0 for "does not exist" so other sizes are incremented by 1 */
+			uint32_t size = indices[i].value.exists() ? indices[i].value.size() + 1 : 0;
+			indices[i].modified = false;
+			buffer << size;
+		}
+		offset = count * sizeof(uint32_t);
+		for(size_t i = 0; i < count; i++)
+			buffer.append(indices[i].value);
+		modified = false;
+		resized = false;
+		base = buffer;
+	}
+	return base;
+}
---
+++ index_blob.h
@@ -0,0 +1,62 @@
+/* This file is part of Toilet. Toilet is copyright 2007-2009 The Regents
+ * of the University of California. It is distributed under the terms of
+ * version 2 of the GNU GPL. See the file LICENSE for details. */
+
+#ifndef __INDEX_BLOB_H
+#define __INDEX_BLOB_H
+
+#include <assert.h>
+
+#ifndef __cplusplus
+#error index_blob.h is a C++ header file
+#endif
+
+#include "blob.h"
+
+class index_blob
+{
+public:
+	index_blob(size_t count);
+	index_blob(size_t count, const blob & x);
+	index_blob(const index_blob & x);
+	index_blob & operator=(const index_blob & x);
+	
+	inline blob get(size_t index) const
+	{
+		assert(index < count);
+		return indices[index].value;
+	}
+	
+	inline int set(size_t index, const blob & value)
+	{
+		assert(index < count);
+		if(value.size() != indices[index].value.size() ||
+		   value.exists() != indices[index].value.exists())
+			resized = true;
+		modified = true;
+		indices[index].value = value;
+		indices[index].modified = true;
+	}
+	
+	blob flatten() const;
+	
+	inline ~index_blob()
+	{
+		delete[] indices;
+	}
+	
+private:
+	struct sub
+	{
+		blob value;
+		bool modified;
+		inline sub() : modified(false) {}
+	};
+	
+	mutable blob base;
+	mutable bool modified, resized;
+	sub * indices;
+	size_t count;
+};
+
+#endif /* __INDEX_BLOB_H */
