4720a888a4ac3fa7c34524e6d55767177f193988
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2009-02-25 19:13:21 -0800

    Use a memory_dtable rather than journal_dtable for the exceptions.

--- exception_dtable.cpp
+++ exception_dtable.cpp
@@ -7,7 +7,7 @@
 #include "openat.h"
 
 #include "util.h"
-#include "journal_dtable.h"
+#include "memory_dtable.h"
 #include "dtable_wrap_iter.h"
 #include "exception_dtable.h"
 
@@ -336,10 +336,8 @@ private:
 int exception_dtable::create(int dfd, const char * file, const params & config, dtable::iter * source, const ktable * shadow)
 {
 	int excp_dfd, r;
-	sys_journal alt_journal;
-	journal_dtable alt_jdt;
+	memory_dtable alt_mdt;
 	reject_iter * handler;
-	sys_journal::listener_id id;
 	params base_config, alt_config;
 	blob reject_value;
 	const dtable_factory * base = dtable_factory::lookup(config, "base");
@@ -368,20 +366,14 @@ int exception_dtable::create(int dfd, const char * file, const params & config, 
 		goto fail_open;
 	/* we should really save the reject_value in a meta file here */
 	
-	r = alt_journal.init(excp_dfd, "alt_journal", true);
+	/* we'll always be appending, but it's faster if we say false here */
+	r = alt_mdt.init(source->key_type(), false, true);
 	if(r < 0)
-		goto fail_journal;
-	id = sys_journal::get_unique_id();
-	if(id == sys_journal::NO_ID)
-		goto fail_id;
-	/* we'll always be appending, since we'll be reading the data in order */
-	r = alt_jdt.init(source->key_type(), id, true, &alt_journal);
-	if(r < 0)
-		goto fail_id;
+		goto fail_mdt;
 	if(source->get_blob_cmp())
-		alt_jdt.set_blob_cmp(source->get_blob_cmp());
+		alt_mdt.set_blob_cmp(source->get_blob_cmp());
 	
-	handler = new reject_iter(source, &alt_jdt, reject_value);
+	handler = new reject_iter(source, &alt_mdt, reject_value);
 	if(!handler)
 		goto fail_handler;
 	
@@ -390,26 +382,22 @@ int exception_dtable::create(int dfd, const char * file, const params & config, 
 		goto fail_base;
 	
 	/* no shadow - this only has exceptions */
-	r = alt->create(excp_dfd, "alt", alt_config, &alt_jdt, NULL);
+	r = alt->create(excp_dfd, "alt", alt_config, &alt_mdt, NULL);
 	if(r < 0)
 		goto fail_alt;
 	
 	delete handler;
-	alt_jdt.deinit(true);
-	alt_journal.deinit(true);
+	alt_mdt.deinit();
 	close(excp_dfd);
 	return 0;
 	
-	/* FIXME: this should just rm_r delete stuff */
 fail_alt:
 	util::rm_r(excp_dfd, "base");
 fail_base:
 	delete handler;
 fail_handler:
-	alt_jdt.deinit(true);
-fail_id:
-	alt_journal.deinit(true);
-fail_journal:
+	alt_mdt.deinit();
+fail_mdt:
 	close(excp_dfd);
 fail_open:
 	unlinkat(dfd, file, AT_REMOVEDIR);
