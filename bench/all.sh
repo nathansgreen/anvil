ac00b6da7a8cd49694766c396b2ef1793b4216a1
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2010-02-01 04:36:30 -0800

    Make it easier to run individual benchmarks.

--- bench/all.sh
+++ bench/all.sh
@@ -3,16 +3,7 @@
 [ -d bench ] || exit 1
 
 source bench/test.sh
-
-# excp_excp_array_x1k takes a really long time and is listed last so it can be run fewer times easily
-TESTS_N=(excp_simple    excp_btree_simple excp_array      excp_excp_array excp_excp_fixed excp_excp_btree_fixed abort        uniq           partition       overlay bloom    oracle                    journal acid         excp_btree_simple_x1k excp_excp_array_x1k)
-CMDS_N=("exdtable perf" "exdtable perf"   "exdtable perf" "exdtable perf" "exdtable perf" "exdtable perf"       "abort perf" "udtable perf" "kddtable perf" odtable bfdtable "oracle${NL}oracle bloom" jdtable "rwatx perf" "exdtable perf"       "exdtable perf")
-
-TESTS_1=(tpch_run_row                  tpch_run_col                      tpch_create_fg_st          tpch_create_fg             tpch_create_bg2            tpch_create_bg)
-CMDS_1=("tpchtype row${NL}tpchtest $N" "tpchtype column${NL}tpchtest $N" "tpchtype row${NL}tpchgen" "tpchtype row${NL}tpchgen" "tpchtype row${NL}tpchgen" "tpchtype row${NL}tpchgen")
-
-TESTS_S=(tpch_create_row                        tpch_create_col)
-CMDS_S=("tpchtype row${NL}tpchgen${NL}tpchopen" "tpchtype column${NL}tpchgen${NL}tpchopen")
+source bench/tests.sh
 
 if [ "$1" = "--setup" ]
 then
--- bench/one.sh
+++ bench/one.sh
@@ -2,22 +2,36 @@
 
 DO_N=no
 
-if [ "$1" = "-N" ]
-then
-	DO_N=yes
-	shift
-fi
-
 [ -d bench ] || exit 1
 
 source bench/test.sh
+source bench/tests.sh
 
 [ -f "bench/$1.patch" ] || exit 2
-[ $# = 2 ] || exit 3
+
+CMD=
+function find_command()
+{
+	local i=0
+	eval "local max=\${#TESTS_$1[*]}"
+	while [ $i -lt $max ]
+	do
+		local name="TESTS_$1[$i]"
+		if [ "${!name}" = "$2" ]
+		then
+			name="CMDS_$1[$i]"
+			CMD="${!name}"
+			return 0
+		fi
+		i=$((i + 1))
+	done
+	return 1
+}
+find_command N "$1" && DO_N=yes || find_command 1 "$1" || find_command S "$1" || exit 3
 
 if [ "$DO_N" = "yes" ]
 then
-	run_test_N "$1" "$2"
+	run_test_N "$1" "$CMD"
 else
-	run_test_1 "$1" "$2"
+	run_test_1 "$1" "$CMD"
 fi
