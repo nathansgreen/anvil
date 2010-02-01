aec326af4f5dd70fd5ec7631e930c79e04840400
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2010-02-01 01:19:49 -0800

    Update derive tool to work with the tpch_create_bg2 test.

--- tools/derive.c
+++ tools/derive.c
@@ -4,12 +4,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
 
-static int parse_line(const char * line, double * time, int * rows)
+enum mode {
+	UNKNOWN,  /* haven't seen the first data line yet */
+	DERIVE,   /* take pseudo-derivative, first line had integer data */
+	TIMEBASE  /* only rebase time to zero, first line had real data */
+};
+
+static int parse_line(const char * line, double * time, int * rows, enum mode * mode)
 {
 	/* skip lines that don't start with numbers */
 	if(*line < '0' || '9' < *line)
 		return 0;
+	if(*mode == UNKNOWN)
+	{
+		const char * space = strchr(line, ' ');
+		assert(space);
+		if(strchr(space, '.'))
+			*mode = TIMEBASE;
+		else
+			*mode = DERIVE;
+	}
+	if(*mode == TIMEBASE)
+	{
+		double krows = 0;
+		int r = sscanf(line, "%lf %lf", time, &krows);
+		*rows = rint(krows * 1000);
+		return (r == 3) ? 1 : -1;
+	}
 	return (sscanf(line, "%lf %d", time, rows) == 2) ? 1 : -1;
 }
 
@@ -17,6 +42,7 @@ int main(void)
 {
 	int last_rows = 0, first = 1;
 	double last_time = 0, first_time = 0;
+	enum mode mode = UNKNOWN;
 	char line[64];
 	
 	while(fgets(line, sizeof(line), stdin))
@@ -24,8 +50,19 @@ int main(void)
 		int this_rows;
 		double this_time;
 		
-		if(!parse_line(line, &this_time, &this_rows))
+		if(!parse_line(line, &this_time, &this_rows, &mode))
 			continue;
+		
+		if(mode == TIMEBASE)
+		{
+			if(first)
+			{
+				first_time = this_time;
+				first = 0;
+			}
+			printf("%lg %lg\n", this_time - first_time, (double) this_rows / 1000);
+			continue;
+		}
 		
 		if(first)
 		{
@@ -38,7 +75,7 @@ int main(void)
 		
 		last_rows = this_rows - last_rows;
 		last_time = this_time - last_time;
-		printf("%lg %lg\n", this_time - first_time, last_rows / last_time);
+		printf("%lg %lg\n", this_time - first_time, last_rows / (last_time * 1000));
 		
 		last_rows = this_rows;
 		last_time = this_time;
